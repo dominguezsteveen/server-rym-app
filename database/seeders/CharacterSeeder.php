393267112d842bc25bb550c7f2fd91a541fc9a81
sdominguezcei <sdominguez@indoamericana.edu.co>
2023-08-28 07:28:29 -0500

    bajando cantidad de migracion para characters

--- database/seeders/CharacterSeeder.php
+++ database/seeders/CharacterSeeder.php
@@ -14,26 +14,7 @@ class CharacterSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 1; $i <= 413; $i++) {
-            $client = new Client(['verify' => false]);
-            $response = $client->get("https://rickandmortyapi.com/api/character/" . $i);
-            $data = json_decode($response->getBody());
-
-            $character = new Character();
-            $character->id = $data->id;
-            $character->name = "$data->name";
-            $character->status = "$data->status";
-            $character->species = $data->species;
-            $character->type = " $data->type";
-            $character->gender = $data->gender;
-            $character->origin = json_encode($data->origin);
-            $character->location = json_encode($data->location);
-            $character->image = $data->image;
-            $character->episode = json_encode($data->episode);
-            $character->url = $data->url;
-            $character->save();
-        }
-        for ($i = 414; $i <= 826; $i++) {
+        for ($i = 1; $i <= 68; $i++) {
             $client = new Client(['verify' => false]);
             $response = $client->get("https://rickandmortyapi.com/api/character/" . $i);
             $data = json_decode($response->getBody());
